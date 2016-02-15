c0d9558de5d4bcb2c71bd812b090ad0050813c21
周铭 <297236646@qq.com>
2016-02-15 14:25:31 +0800

    add Models and create user table

--- database/migrations/2016_02_15_062059_create_user_table.php
+++ database/migrations/2016_02_15_062059_create_user_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,10 +12,10 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token')->index();
-            $table->timestamp('created_at');
+        Schema::table('users', function (Blueprint $table) {
+            //创建用户表
+            $table->increments('id');
+
         });
     }
 
@@ -26,6 +26,8 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('password_resets');
+        Schema::table('create', function (Blueprint $table) {
+            //
+        });
     }
 }
